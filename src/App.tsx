d6baa44945fa18e61d8b6cc7397412f679a18ad6
Kris Kamikawa <kris.kamikawa@gmail.com>
2020-08-19 22:25:19 -0600

    add form elements and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import "./App.css";
 import { useMachine } from "@xstate/react";
 import loginMachine from "./machine";
 
 function App() {
+  const [current, send] = useMachine(loginMachine);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    if (name === 'username') {
+      send('')
+    }
+    if (name === 'password') {
+      send('')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">X-State Practice: Login</header>
+      <form onSubmit={() => {}}>
+      <h1 className="App-header">X-State Practice: Login</h1>
+
+      <div className="input-container">
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        type="text"
+        name="username"
+        value={current.context.username}
+        onChange={handleChange}
+      />
+      </div>
+
+      <div className="input-container">
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="text"
+        name="password"
+        value={current.context.password}
+        onChange={handleChange}
+      />
+      </div>
+
+      <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
